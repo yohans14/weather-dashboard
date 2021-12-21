8bb313cb62452533d664364aa3962383fc298bc0
yohans14 <yohans14@yahoo.com>
2021-12-21 17:06:28 -0600

    ajax api get

---
+++ assets/js/weather.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+    // EventListener for search input
+    $("#searchbtn").on("click", function (e) {
+      e.preventDefault();
+      // GNews api url with query input.text
+      var query = $("#cityName").val();
+      var url ="api.openweathermap.org/data/2.5/forecast?" +
+      "q=" + query +"&units=imperial&appid=0ac1781e4e58355720795bec8018d18e"
+
+      if (query !== "") {
+        $.ajax({
+          url: url,
+          method: "GET",
+          dataType: "json",
+  
+          // loader UI display until news url return and hide after complete
+          beforeSend: function () {
+            $("#loader").show();
+          },
+          complete: function () {
+            $("#loader").hide();
+          },
+          // if ajax request return true the array data passed through for() loop
+          success: function (weather) {
+            let output = "";
+            let currentweather = weather.current;
+
+            console.log(weather)
+
+            for (var i in weather.current) {
+              output += `
+              
+                   `;
+            }if(output !==""){
+              $("#currentWeather").html(output);
+            }
+  
+          },
+          error: function () {
+            M.toast({
+              html: "There is an error, please try again",
+              classes: "red rounded",
+            });
+          },
+        });
+      } 
+    });
+  });
+  
