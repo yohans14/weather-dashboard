ecef56d90716efe91f6129e02a3ca91232800742
yohans14 <yohans14@yahoo.com>
2022-01-02 04:25:58 -0600

    readme

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,49 +1,19 @@
-$(document).ready(function () {
-    // EventListener for search input
-    $("#searchbtn").on("click", function (e) {
-      e.preventDefault();
-      // GNews api url with query input.text
-      var query = $("#cityName").val();
-      var url ="https://api.openweathermap.org/data/2.5/forecast?" +
-      "q=" + query +
-      "&units=imperial&appid=0ac1781e4e58355720795bec8018d18e"
-      if (query !== "") {
-        $.ajax({
-          url: url,
-          method: "GET",
-          dataType: "json",
-  
-          // loader UI display until news url return and hide after complete
-          beforeSend: function () {
-            $("#loader").show();
-          },
-          complete: function () {
-            $("#loader").hide();
-          },
-          // if ajax request return true the array data passed through for() loop
-          success: function (weather) {
-            let output = "";
-            let currentweather = weather.list;
+let weather = { 
+fetchweather: function (city){
+fetch("https://api.openweathermap.org/data/2.5/forecast?" +
+"q=" + city +
+"&units=imperial&appid=0ac1781e4e58355720795bec8018d18e")
 
-            console.log(weather)
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data));
+      },
+      
+           displayWeather: function(data) {
+             const { name } = data;
+            const { icone, description } = data.weather[0];
+            const { temp, humidity } = data.main;
+            const { speed } = data.wind;
 
-            for (var i in weather.current) {
-              output += `
-              
-                   `;
-            }if(output !==""){
-              $("#currentWeather").html(output);
-            }
-  
-          },
-          error: function () {
-            M.toast({
-              html: "There is an error, please try again",
-              classes: "red rounded",
-            });
-          },
-        });
-      } 
-    });
-  });
-  
+            console.log(name, icone, description, temp, speed);
+          }
+        }  
